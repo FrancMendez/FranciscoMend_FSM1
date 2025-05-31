4e907b45ed2b7913ee0994f09e8beba38e133a91
FrancMendez <fmendezs@miumg.edu.gt>
2025-05-31 16:10:42 -0600

    Add files via upload

---
+++ test/test.v
@@ -0,0 +1,9 @@
+`default_nettype none
+
+module test;
+    // Tiny Tapeout testbench template
+    initial begin
+        $display("Tiny Tapeout test started.");
+        $finish;
+    end
+endmodule
